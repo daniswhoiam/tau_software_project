e16720c5514083c8e50792a05bb0804c2d79f994
daniswhoiam <daniil.belazovschi@gmail.com>
2022-11-23 19:30:52 +0200

    Final

--- id1_id2_bc.c
+++ id1_id2_bc.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <math.h>
 
-int i = 0;
+int i;
 
 int source_base = 0;
-int target_base = 0;
+int target_base;
 
 int in_decimal = 0;
 int in_target = 0;
 
+/* SETTING THE SOURCE BASE */
+void set_base(int *base)
+{
+  char c;
+  if ((c = getchar()) != '\n')
+  {
+    set_base(base);
+    *base += (int)((c - '0') * pow(10, i));
+    i++;
+  }
+  return;
+}
+
+/* TRANSLATING INPUT FROM SOURCE BASE TO BASE 10 */
 int to_decimal(void)
 {
   char c;
@@ -41,30 +55,45 @@ int to_decimal(void)
   return input_legit;
 }
 
+/* TRANSLATING BASE 10 TO TARGET BASE*/
 void to_target(void)
 {
   int inter;
   int mod;
   if (in_decimal != 0)
   {
+    /* How many times does target_base fully fit into in_decimal */
     inter = in_decimal / pow(target_base, i);
+    /* What remains */
     mod = in_decimal % ((int)pow(target_base, i + 1));
   }
   else
   {
-    /* BASE CASE */
+    /* BASE CASE in_decimal == 0 */
     return;
   }
 
   if (inter >= target_base)
   {
+    /* 
+    * If target_base fits into in_decimal more than it's own value
+    * We need to go to the next power (target_base^(i))
+    */
     in_decimal = in_decimal - mod;
     i++;
     to_target();
+    /*
+    * After some recursive operations we need to calculate mod again
+    * Because then we have already taken away the part of "inter"
+    * Thas was too much to represent at this stage of i
+    */
     mod = inter % ((int)pow(target_base, i));
   }
   else if (inter < target_base)
   {
+    /*
+    * When we don't need to go to a higher power dimension
+    */
     if (inter < 9 && inter >= 0)
     {
       printf("%d", inter);
@@ -73,6 +102,9 @@ void to_target(void)
     {
       printf("%c", inter + 87);
     }
+    /**
+     * Necessary to reach base case confidently
+    */
     if (in_decimal != mod)
     {
       in_decimal = mod;
@@ -81,6 +113,9 @@ void to_target(void)
     {
       in_decimal = 0;
     }
+    /*
+    * We go down a dimension and start from the beginning
+    */
     i--;
     to_target();
     return;
@@ -97,52 +132,32 @@ void to_target(void)
   return;
 }
 
-void set_base(int *base)
-{
-  char c;
-  if ((c = getchar()) != '\n')
-  {
-    set_base(base);
-    *base += (int)((c - '0') * pow(10, i));
-    i++;
-  }
-  return;
-}
-
 int main(void)
 {
-  printf("Enter the source base:\n");
-  set_base(&source_base);
-  while (source_base < 2 || source_base > 16)
+  do
   {
-    printf("%d", source_base);
-    printf("Invalid source base!\n");
-    source_base = 0;
     i = 0;
+    source_base = 0;
     printf("Enter the source base:\n");
     set_base(&source_base);
-  }
+  } while ((source_base < 2 || source_base > 16) && printf("Invalid source base!\n"));
   printf("The source base is: %d\n", source_base);
-  i = 0;
-  printf("Enter the target base:\n");
-  set_base(&target_base);
-  while (target_base < 2 || target_base > 16)
+
+  do
   {
-    printf("Invalid target base!\n");
-    target_base = 0;
     i = 0;
+    target_base = 0;
     printf("Enter the target base:\n");
     set_base(&target_base);
-  }
+  } while ((target_base < 2 || target_base > 16) && printf("Invalid target base!\n"));
   printf("The target base is: %d\n", target_base);
-  i = 0;
-  printf("Enter a number in base %d\n", source_base);
-  while (!to_decimal())
+
+  do
   {
-    printf("Invalid input number!\n");
     i = 0;
     printf("Enter a number in base %d\n", source_base);
-  }
+  } while (!to_decimal() && printf("Invalid input number!\n"));
+
   i = 0;
   printf("The entered number in base %d is: ", target_base);
   to_target();
