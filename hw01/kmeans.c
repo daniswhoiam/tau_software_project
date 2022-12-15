9e4dc7928c9d7f080e1569c720d48355c184a535
daniswhoiam <daniil.belazovschi@gmail.com>
2022-12-15 12:45:50 +0200

    Reading input works

--- hw01/kmeans.c
+++ hw01/kmeans.c
@@ -2,54 +2,119 @@
 #include <math.h>
 #include <stdlib.h>
 
-
-
 int main(int argc, char **argv)
 {
   long K;
   long iter;
-  int N;
   FILE *textfile;
-  double d1, d2, d3;
- /* Import and check number of data points*/
+
+  /* For numbers import*/
+  int r = 1, c = 1;
+  double *p, **arr;
+  int count = 0;
+  int i = 0, j = 0, k;
+  char line[256];
+
+  /* Import and check number of data points*/
 
   /* Validate Input */
-  if (argc < 2 || argc > 3)
+  if (argc < 3 || argc > 4)
   {
     printf("Wrong number of arguments!");
     return 1;
   }
 
   textfile = fopen("./input_1.txt", "r");
-  if (textfile == NULL) {
+  if (textfile == NULL)
+  {
     printf("An error has occured.\n");
     return 1;
   }
 
-  fscanf(textfile, "%lf,%lf,%lf\n", &d1, &d2, &d3);
-  while(!feof (textfile)) {
-    printf("%f,%f,%f\n", d1, d2, d3);
-    fscanf(textfile, "%lf,%lf,%lf\n", &d1, &d2, &d3);
-    N = N + 3;
+  /*Read first row, determine column number*/
+  while (fgets(line, sizeof(line), textfile))
+  {
+    char *l = line;
+    double temp;
+    int n;
+    while (sscanf(l, "%lf,%n", &temp, &n) == 1)
+    {
+      /*
+      printf("%f\n", temp);
+      p[count] = temp;
+      p = realloc(p, (count + 2) * sizeof(double));
+      */
+      if (r == 1)
+        c++;
+      l += n;
+      count++;
+    }
+    /*
+    arr[r] = arr[r - 1] + c * sizeof(double);
+    */
+    r++;
   }
+  r = r - 1;
+  c = c - 1;
   fclose(textfile);
 
+  /*Double Array erstellen (columns)*/
+  p = calloc(r * c, sizeof(double));
+  /*Array für double Arrays erstellen (rows)*/
+  arr = calloc(r, sizeof(double *));
+  for (k = 0; k < r; k++)
+  {
+    arr[k] = p + k * c;
+  }
+
+  textfile = fopen("./input_1.txt", "r");
+  if (textfile == NULL)
+  {
+    printf("An error has occured.\n");
+    return 1;
+  }
+
+  while (fgets(line, sizeof(line), textfile))
+  {
+    char *l = line;
+    double temp;
+    int n;
+    while (sscanf(l, "%lf,%n", &temp, &n) == 1)
+    {
+      arr[i][j] = temp;
+      j++;
+      l += n;
+    }
+    j = 0;
+    i++;
+  }
+
+  fclose(textfile);
+  free(p);
+  free(arr);
+
+  /* Set number of clusters */
   K = strtol(argv[1], NULL, 10);
-  if (K < 2 || K > (N - 1)) {
+  if (K < 2 || K > (count - 1))
+  {
     printf("Invalid number of clusters!");
     return 1;
   }
-  if (argc == 3) {
+
+  /* Set number of iterations */
+  if (argc == 4)
+  {
     iter = strtol(argv[2], NULL, 10);
-    if (iter < 2 || iter > 999) {
+    if (iter < 2 || iter > 999)
+    {
       printf("Invalid maximum iteration!");
       return 1;
     }
-  } else {
+  }
+  else
+  {
     iter = 200;
   }
-
-  /* Set number of clusters */
 
   /* Initialize centroids */
 
