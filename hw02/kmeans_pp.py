6ee837e081b8f4d61f5f8b6aa7a7f15d55c8c314
daniswhoiam <daniil.belazovschi@gmail.com>
2023-01-06 15:34:20 +0200

    Initial

---
+++ hw02/kmeans_pp.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import math
+
+if __name__ == "__main__":
+  
+  # Check number of args
+  if len(sys.argv) < 5 or len(sys.argv) > 6:
+    print("Invalid number of arguments!")
+    exit()
