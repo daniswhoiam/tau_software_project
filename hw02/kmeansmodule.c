fa0fb675d98e4a1a5c3a613dbd2927acac311882
daniswhoiam <daniil.belazovschi@gmail.com>
2023-01-09 11:22:43 +0200

    first v new c code

--- hw02/kmeansmodule.c
+++ hw02/kmeansmodule.c
@@ -47,115 +47,30 @@ int isNatNumber(char number[])
   return 1;
 }
 
-int main(int argc, char **argv)
+int fit (double **data, double **centroids, int K, int iter, double epsilon)
 {
-  long K;
-  long iter;
-  FILE *textfile;
-  char *filename;
+  /* Dimensions */
+  int r, c, count;
 
-  /* For numbers import*/
-  int r = 1, c = 1;
-  double *p, **arr;
-  int count = 0;
-  int i = 0, j = 0, k;
-  char line[256];
+  /* Iterators */
+  int i = 0, j = 0, k = 0;
 
   /* For centroids */
-  double *p_c, **arr_c;
   double **rel, ***arr_rel;
 
   /* For loop */
   int iteration_number = 0;
-  double epsilon = 0.001;
   int treshold_reached = 0;
   double *prev_centroid;
   double *new_centroid;
   double *centroid_sum;
 
-  /* Validate number of args */
-  if (argc < 3 || argc > 4)
-  {
-    printf("Wrong number of arguments!");
-    return 1;
-  }
-
-  /* Get filename */
-  if (argc == 3)
-  {
-    filename = argv[2];
-  }
-  else
-  {
-    filename = argv[3];
-  }
-
-  textfile = fopen(filename, "r");
-  if (textfile == NULL)
-  {
-    printf("An error has occured.\n");
-    return 1;
-  }
-
-  /* Import Input */
-
-  /*Read first row, determine column number*/
-  while (fgets(line, sizeof(line), textfile))
-  {
-    char *l = line;
-    double temp;
-    int n;
-    while (sscanf(l, "%lf,%n", &temp, &n) == 1)
-    {
-      if (r == 1)
-        c++;
-      l += n;
-      count++;
-    }
-    r++;
-  }
-  r = r - 1;
-  c = c - 1;
-  fclose(textfile);
-
-  p = calloc(r * c, sizeof(double));
-  arr = calloc(r, sizeof(double *));
-  for (k = 0; k < r; k++)
-  {
-    arr[k] = p + k * c;
-  }
-
-  textfile = fopen(filename, "r");
-  if (textfile == NULL)
-  {
-    printf("An error has occured.\n");
-    return 1;
-  }
-
-  while (fgets(line, sizeof(line), textfile))
-  {
-    char *l = line;
-    double temp;
-    int n;
-    while (sscanf(l, "%lf,%n", &temp, &n) == 1)
-    {
-      arr[i][j] = temp;
-      j++;
-      l += n;
-    }
-    j = 0;
-    i++;
-  }
-
-  fclose(textfile);
+  /* Calculating Dimensions */
+  c = sizeof(data[0]) / sizeof(data[0][0]);
+  r = sizeof(data) / sizeof(data[0]);
+  count = c * r;
 
   /* Set number of clusters */
-  if (isNatNumber(argv[1]) == 0)
-  {
-    printf("Invalid number of clusters!\n");
-    return 1;
-  }
-  K = (int)strtol(argv[1], NULL, 10);
   if (K < 2 || K > (count - 1))
   {
     printf("Invalid number of clusters!\n");
@@ -163,38 +78,14 @@ int main(int argc, char **argv)
   }
 
   /* Set number of iterations */
-  if (argc == 4)
+  if (iter < 2 || iter > 999)
   {
-    if (isNatNumber(argv[2]) == 0)
-    {
-      printf("Invalid maximum iteration!\n");
-      return 1;
-    }
-    iter = strtol(argv[2], NULL, 10);
-    if (iter < 2 || iter > 999)
-    {
-      printf("Invalid maximum iteration!\n");
-      return 1;
-    }
+    printf("Invalid maximum iteration!\n");
+    return 1;
   }
   else
   {
     iter = 200;
-  }
-
-  /* Initialize centroids */
-  p_c = calloc(K * c, sizeof(double));
-  arr_c = calloc(K, sizeof(double *));
-  for (k = 0; k < K; k++)
-  {
-    arr_c[k] = p_c + k * c;
-  }
-  for (i = 0; i < K; i++)
-  {
-    for (j = 0; j < c; j++)
-    {
-      arr_c[i][j] = arr[i][j];
-    }
   }
 
   /* Initiate centroid - point arrays*/
@@ -226,7 +117,7 @@ int main(int argc, char **argv)
         double inter_diff = 0.0;
         for (j = 0; j < c; j++)
         {
-          inter_sum += pow((arr[i][j] - arr_c[k][j]), 2.0);
+          inter_sum += pow((data[i][j] - centroids[k][j]), 2.0);
         }
         inter_diff = sqrt(inter_sum);
         /* Save difference and nearest centroid*/
@@ -236,8 +127,8 @@ int main(int argc, char **argv)
           cent = k;
         }
       }
-      arr_rel[i][0] = arr[i];
-      arr_rel[i][1] = arr_c[cent];
+      arr_rel[i][0] = data[i];
+      arr_rel[i][1] = centroids[cent];
     }
 
     /* Update centroids */
@@ -248,12 +139,12 @@ int main(int argc, char **argv)
 
       for (j = 0; j < c; j++)
       {
-        prev_centroid[j] = arr_c[k][j];
+        prev_centroid[j] = centroids[k][j];
       }
 
       for (i = 0; i < r; i++)
       {
-        if (arr_rel[i][1] == arr_c[k])
+        if (arr_rel[i][1] == centroids[k])
         {
           for (j = 0; j < c; j++)
           {
@@ -265,12 +156,12 @@ int main(int argc, char **argv)
       for (j = 0; j < c; j++)
       {
         centroid_sum[j] = centroid_sum[j] / k_count;
-        arr_c[k][j] = centroid_sum[j];
+        centroids[k][j] = centroid_sum[j];
       }
 
       for (j = 0; j < c; j++)
       {
-        new_centroid[j] = arr_c[k][j];
+        new_centroid[j] = centroids[k][j];
       }
 
       for (j = 0; j < c; j++)
@@ -290,32 +181,13 @@ int main(int argc, char **argv)
     iteration_number++;
   }
 
-  /* Print out Result */
-  for (k = 0; k < K; k++)
-  {
-    for (j = 0; j < c; j++)
-    {
-      if (j == (c - 1))
-      {
-        printf("%.4f", arr_c[k][j]);
-      }
-      else
-      {
-        printf("%.4f,", arr_c[k][j]);
-      }
-    }
-    printf("\n");
-  }
-
-  free(p);
-  free(arr);
-  free(p_c);
-  free(arr_c);
+  // free(arr);
+  // free(centroids);
   free(rel);
   free(arr_rel);
   free(centroid_sum);
   free(prev_centroid);
   free(new_centroid);
 
-  return 0;
+  return centroids;
 }
