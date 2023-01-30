77785877341bd25cd6f20ce1a46086515081e81d
daniswhoiam <daniil.belazovschi@gmail.com>
2023-01-30 14:49:17 +0200

    Make laplacian matrix

--- project/spkmeans.py
+++ project/spkmeans.py
@@ -1,5 +1,34 @@
 import sys
+import math
 import numpy as np
+import pandas as pd
+
+
+def sq_eq_dist(point1, point2):
+    return np.linalg.norm(point1 - point2) ** 2
+
+
+def make_wadjm(data, N):
+    wadjm = np.zeros((N, N))
+
+    for i in range(0, N):
+        for j in range(0, N):
+            if i != j:
+                point1 = data[i]
+                point2 = data[j]
+                wadjm[i][j] = math.exp(-(sq_eq_dist(point1, point2) / 2))
+
+    return wadjm
+
+
+def make_diagdem(wadjm, N):
+    diagdem = np.zeros((N, N))
+
+    for i in range(0, N):
+        for j in range(0, N):
+            diagdem[i][i] += wadjm[i][j]
+
+    return diagdem
 
 if __name__ == "__main__":
 
@@ -33,3 +62,14 @@ if __name__ == "__main__":
         except:
             print("An error has occured!")
             exit()
+
+    # Make Weight Adjusted Matrix
+    wadjm = make_wadjm(matrix, N)
+
+    # Make Diagonal Degree Matrix
+    diagdem = make_diagdem(wadjm, N)
+
+    # Make Laplacian
+    laplac = np.subtract(diagdem, wadjm)
+
+    print(laplac)
