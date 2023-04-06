2bc57f1d75c1143098b5764cedd5aebb1a8d5fc5
daniswhoiam <daniil.belazovschi@gmail.com>
2023-04-06 13:27:52 +0200

    working python jacobi

--- project/spkmeans.py
+++ project/spkmeans.py
@@ -45,9 +45,32 @@ def calc_max_offdiag(A):
     return i, j
 
 
-def jacobi_eigenvalue(A, tol=1e-255, max_iter=100):
+def rotate_matrix(A, i, j):
+    n = A.shape[0]
+    # Numbers for Rotation matrix
+    theta = (A[j, j] - A[i, i]) / (2 * A[i, j])
+    t = 1 / (abs(theta) + math.sqrt(theta**2 + 1))
+    if theta < 0.0:
+        t *= -1
+    c = 1 / (math.sqrt(t**2 + 1))
+    s = t * c
+
+    A_new = A.copy()
+
+    for r in range(n):
+        if r != i and r != j:
+            A_new[r][i] = c * A[r][i] - s * A[r][j]
+            A_new[r][j] = c * A[r][j] + s * A[r][i]
+    A_new[i][i] = c**2 * A[i][i] + s**2 * A[j][j] - 2 * s * c * A[i][j]
+    A_new[j][j] = s**2 * A[i][i] + c**2 * A[j][j] + 2 * s * c * A[i][j]
+    A_new[i][j] = 0.0
+
+    return A_new
+
+""" def jacobi_eigenvalue(A, tol=1.0e-10, max_iter=1000):
     n = A.shape[0]
     V = np.identity(n)
+    P = A.copy()
 
     # Maximum iteration condition
     for m in range(max_iter):
@@ -57,13 +80,12 @@ def jacobi_eigenvalue(A, tol=1e-255, max_iter=100):
         # Numbers for Rotation matrix
         theta = (A[j, j] - A[i, i]) / (2 * A[i, j])
         t = 1 / (abs(theta) + math.sqrt(theta**2 + 1))
-        if theta < 0:
+        if theta < 0.0:
             t *= -1
         c = 1 / (math.sqrt(t**2 + 1))
         s = t * c
 
         # Rotation matrix
-        P = np.identity(n)
         P[i, i], P[j, j] = c, c
         P[i, j], P[j, i] = -s, s
 
@@ -71,7 +93,7 @@ def jacobi_eigenvalue(A, tol=1e-255, max_iter=100):
         off_A = np.sum(np.abs(A - np.diag(np.diag(A))) ** 2)
 
         # Rotation
-        A = P.T @ A @ P
+        A = rotate_matrix(A, i, j)
         V = V @ P
 
         # For tolerance breaking condition
@@ -80,8 +102,59 @@ def jacobi_eigenvalue(A, tol=1e-255, max_iter=100):
         if off_A - off_A_new <= tol:
             break
 
-    return np.diag(A), V
+    return np.diag(A), V """
 
+def jacobi(a,tol = 1.0e-6): # Jacobi method
+
+    def maxElem(a): # Find largest off-diag. element a[k,l]
+        n = len(a)
+        aMax = 0.0
+        for i in range(n-1):
+            for j in range(i+1,n):
+                if abs(a[i,j]) >= aMax:
+                    aMax = abs(a[i,j])
+                    k = i; l = j
+        return aMax,k,l
+
+    def rotate(a,p,k,l): # Rotate to make a[k,l] = 0
+        n = len(a)
+        aDiff = a[l,l] - a[k,k]
+        if abs(a[k,l]) < abs(aDiff)*1.0e-36: t = a[k,l]/aDiff
+        else:
+            phi = aDiff/(2.0*a[k,l])
+            t = 1.0/(abs(phi) + math.sqrt(phi**2 + 1.0))
+            if phi < 0.0: t = -t
+        c = 1.0/math.sqrt(t**2 + 1.0); s = t*c
+        tau = s/(1.0 + c)
+        temp = a[k,l]
+        a[k,l] = 0.0
+        a[k,k] = a[k,k] - t*temp
+        a[l,l] = a[l,l] + t*temp
+        for i in range(k):      # Case of i < k
+            temp = a[i,k]
+            a[i,k] = temp - s*(a[i,l] + tau*temp)
+            a[i,l] = a[i,l] + s*(temp - tau*a[i,l])
+        for i in range(k+1,l):  # Case of k < i < l
+            temp = a[k,i]
+            a[k,i] = temp - s*(a[i,l] + tau*a[k,i])
+            a[i,l] = a[i,l] + s*(temp - tau*a[i,l])
+        for i in range(l+1,n):  # Case of i > l
+            temp = a[k,i]
+            a[k,i] = temp - s*(a[l,i] + tau*temp)
+            a[l,i] = a[l,i] + s*(temp - tau*a[l,i])
+        for i in range(n):      # Update transformation matrix
+            temp = p[i,k]
+            p[i,k] = temp - s*(p[i,l] + tau*p[i,k])
+            p[i,l] = p[i,l] + s*(temp - tau*p[i,l])
+        
+    n = len(a)
+    maxRot = 5*(n**2)       # Set limit on number of rotations
+    p = np.identity(n)*1.0     # Initialize transformation matrix
+    for i in range(maxRot): # Jacobi rotation loop 
+        aMax,k,l = maxElem(a)
+        if aMax < tol: return np.diagonal(a),p
+        rotate(a,p,k,l)
+    print('Jacobi method did not converge')
 
 if __name__ == "__main__":
 
@@ -126,7 +199,8 @@ if __name__ == "__main__":
     laplac = np.subtract(diagdem, wadjm)
 
     # Eigenvalues and eigenvectors
-    jacobi_result = jacobi_eigenvalue(laplac)
+    jacobi_result = jacobi(matrix)
     eigenvalues = jacobi_result[0]
     eigenvectors = jacobi_result[1]
     print(eigenvalues)
+    print(eigenvectors)
